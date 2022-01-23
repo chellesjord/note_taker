fdd8909c6873a566aad6ece45d907e9008b20b16
chellesjord <m.kayejordan@gmail.com>
2022-01-23 13:47:09 -0600

    middleware added to server.js

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 //initiate express
 const express = require('express');
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
+const htmlRoute = require('./routes/htmlRoutes');
+const apiRoute = require('./routes/apiRoutes/noteRoute');
 const app = express();
-// const fs = require('fs');
-// const util = require('util');
+
+
+//middleware
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static('./public'));
+app.use('/api/notes', apiRoute);
+app.use('/', htmlRoute);
+
+//API GET Request
+app.get('api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "/db/db.json"))
+});
+
+//API POST Request
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
-  });
+});
 
-//   const { }
