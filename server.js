e20cc9efd939d058ecc92c40172b104c843076cf
chellesjord <m.kayejordan@gmail.com>
2022-01-22 23:45:37 -0600

    add Heroku

---
+++ server.js
@@ -0,0 +1,12 @@
+//initiate express
+const express = require('express');
+const app = express();
+// const PORT =
+// const fs = require('fs');
+// const util = require('util');
+
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+  });
+
+//   const { }
